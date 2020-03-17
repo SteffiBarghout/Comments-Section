27fde1bb51e13f4daa36653997e88e7db4dc6510
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-03-16 21:50:38 -0700

    initial html, links to jquery, firebase, and moment.js and link of js to html, firebase apikey on js file

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDE-MjuF2PEURC-rQVNCrWOkd4F-27V7I4",
+    authDomain: "sw-project1-coronavirus.firebaseapp.com",
+    databaseURL: "https://sw-project1-coronavirus.firebaseio.com",
+    projectId: "sw-project1-coronavirus",
+    storageBucket: "sw-project1-coronavirus.appspot.com",
+    messagingSenderId: "475105567752",
+    appId: "1:475105567752:web:8f72f9bf0f26121dfaddbd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
